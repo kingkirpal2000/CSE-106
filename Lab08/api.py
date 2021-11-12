f46a045321435b9c77867560a4a4681255e1075c
Sabir Kirpal <skirpal2000@gmail.com>
2021-11-11 17:35:59 -0800

    Still working

--- Lab08/api.py
+++ Lab08/api.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, request
 from flask_admin import Admin
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin.contrib.sqla import ModelView
-from flask_login import LoginManager, current_user, login_user
+from flask_login import LoginManager, current_user, login_user, UserMixin, login_required
 
 
 
@@ -20,42 +20,74 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.sqlite"
 
 admin = Admin(app, name='microblog', template_mode='bootstrap3')
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, nullable=False)
+    name = db.Column(db.String, unique=False, nullable = False)
     password = db.Column(db.String, unique=False, nullable=False)
     is_active = True
 
-    def get_id(self):
-        return self.username
+    def __init__(self, id):
+        self.id = id
 
     def check_password(self, inputPassword):
-        print(inputPassword)
-        print(self.password)
         return inputPassword == self.password
 
-admin.add_view(ModelView(User, db.session))
+class Teachers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=False, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, unique=False, nullable=False)
 
+class Classes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    courseName = db.Column(db.String, unique=True, nullable=False)
+    teacher_id = db.Column(db.Integer, db.ForeignKey(Teachers.id), nullable=False)
+    numEnrolled = db.Column(db.Integer, unique=False, nullable=False)
+    capacity = db.Column(db.Integer, unique=False, nullable=False)
+    time = db.Column(db.String, unique=False, nullable=False)
+
+
+
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Classes, db.session))
+admin.add_view(ModelView(Teachers, db.session))
 
 db.create_all()
 db.session.commit()
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.get(user_id)
+    try:
+        u = User.query.get(user_id)
+        return u
+    except:
+        return None
 
 @app.route("/", methods=["GET"])
 def landingPage():
-    return render_template("login.html")
+    if(current_user.is_authenticated):
+        return render_template("dashboard.html")
+    else:
+        return render_template("login.html")
 
 @app.route("/login", methods=["POST"])
 def returningUser():
     user = User.query.filter_by(username=request.json['username']).first()
     if user is not None and user.check_password(request.json['password']):
         login_user(user)
-        return dict(msg = "SUCCESS")
+        print("LOGGED IN USER WITH ID: " + str(user.id))
+        return render_template("dashboard.html")
     else:
         return dict(msg = "FAILED")
+
+@app.route("/dashboard", methods = ["GET"])
+@login_required
+def loadDashboard():
+    return render_template("dashboard.html")
+
+# @app.route("/browseclasses", method=["GET"])
+# def loadClasses():
 
 
 app.run()
